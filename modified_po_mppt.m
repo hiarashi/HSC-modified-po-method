fd784cfc6d8694b36a097919927bf6d67c837d58
hiarashi <81462386+hiarashi@users.noreply.github.com>
2022-04-28 16:49:02 +0300

    Add files via upload
    Added missing year of release

--- modified_po_mppt.m
+++ modified_po_mppt.m
@@ -1,5 +1,5 @@
 %% Modified P&O Maximum Power Point Tracking
-% Copyright Henri Salonen
+% Copyright 2022 Henri Salonen
 %This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
 %as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 %This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
